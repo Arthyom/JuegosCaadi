c2a920127ce85d6ba1f12371d1de22ec01c6c1e9
arthyom <allpipiasaaa@hotmail.com>
2017-06-06 02:05:02 -0500

    clases para una solicitud

---
+++ JuegosCaadi/src/java/Logic_ObjetosBaseDatos/Logic_Solcitud.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic_ObjetosBaseDatos;
+
+/**
+ *
+ * @author frodo
+ */
+public class Logic_Solcitud {
+    
+   // propiedades de la clase Logic_Solicitud
+   public int     idSolicitud ;
+   public String  ObsvSalida ;
+   public String  ObsvEntrada;
+   public String  FechaPeticion;
+   public String  FechaRespuesta;
+   public String  FechaTerminacion;
+   public int     Prf_IdProfesor; 
+
+   // constructor por defecto
+   public Logic_Solcitud(){}
+
+}
